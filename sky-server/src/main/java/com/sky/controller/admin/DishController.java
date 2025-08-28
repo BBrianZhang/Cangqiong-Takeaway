c1cfc0bcee431c555ec1139b772380b03a61fa1b
BBrianZhang <zhang_hfut@icloud.com>
2025-08-28 15:46:23 +0800

    新增小程序相关代码 微信用户登录 商品浏览功能

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -3,7 +3,6 @@ package com.sky.controller.admin;
 import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
-import com.sky.entity.Setmeal;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
@@ -16,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@RestController("adminDishController")
 @RequestMapping("/admin/dish")
 @Api(tags = "菜品相关接口")
 @Slf4j
